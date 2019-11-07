3a0bc3e4106748a16ca0a68d44e9606a2d7a1801
NicDominguez <NicDominguez@users.noreply.github.com>
2019-11-07 10:43:54 -0800

    created conditional error message on email validation

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@
 // Basic Info
 const userName = $('#name')
 const userEmail = $('#mail')
+const userTitleSELECT = $('#title')[0]
 
 //Design and Color Section
 const otherJobRoleINPUT = $('#other-title');
@@ -40,8 +41,6 @@ const registerBTN = $("button[type='submit']")
 userName.focus();
 // Sets Other Job Role Input to hide if JS working
 otherJobRoleINPUT.hide();
-// Removes "Select Theme" option from design select menu
-selectThemeOPTION.remove();
 // Adds "Please select a T-shirt theme" option to color select menu
 colorSELECT.prepend($('<option selected></option>').text("Please select a T-shirt theme"));
 // Hides all colors in the color select menu
@@ -61,6 +60,14 @@ bitCoinDIV.hide()
 ////////////////////////////////////////////////////
 // EVENTS
 ////////////////////////////////////////////////////
+
+userTitleSELECT.addEventListener('change', () => {
+    let value = userTitleSELECT.value
+    if (value === "other") {
+        otherJobRoleINPUT.show()
+    }
+})
+
 
 // Sets correct color options depending on Theme selection
 designSELECT.addEventListener('change', () => {
@@ -142,12 +149,14 @@ creditCardDIV.keyup( (e) => {
 userName.keyup( (e) => {
     let input = e.target
     $(input).next(".error-message").remove()
+    $(input).css("border-color", "initial")
     nameValidation()
 })
 
 userEmail.keyup((e) => {
     let input = e.target
     $(input).next(".error-message").remove()
+    $(input).css("border-color", "initial")
     emailValidation()
 })
 
@@ -155,6 +164,12 @@ registerBTN.click( (event) => {
     event.preventDefault()
     basicInfoValidation()
     creditCardValidation()
+})
+
+$('input').focus( (e) => {
+    let input = e.target
+    $(input).next(".error-message").remove()
+    $(input).css("border-color", "initial")
 })
 
 
@@ -166,65 +181,61 @@ registerBTN.click( (event) => {
 // Payment Validation Functions
 
 const nameValidation = () => {
-    if (/[A-Z][a-zA-Z][^#&<>\"~;$^%{}?]{1,30}$/g.test(userName.val())) {
-        console.log("name true")
+    if (/^\s*$/.test(userName.val())) {
+        console.log("true")
+        insertErrorMessage(userName, "The name field must not be blank")
         return true
     } else {
-        console.log("name false")
-        insertErrorMessage(userName, "The name field cannot be blank")
+        console.log("false")
         return false
     }
 } 
 
 const emailValidation = () => { 
-    if (/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(userEmail.val())) {
-        console.log("email true")
+    if (/^\s*$/.test(userEmail.val())) {
+        insertErrorMessage(userEmail, "The email field must not be blank")
         return true
     } else {
-        console.log("email false")
-        insertErrorMessage(userEmail, "The email address must be properly formatted")
-        return false
+        if (/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(userEmail.val())) {
+            return true
+        } else {
+            insertErrorMessage(userEmail, "The email address must be properly formatted")
+            return false
+        }
     }
 }
+
 const activityValidation = () => {
     let checkedActivityINPUTS = $(activitySection).find("input:checked")
     if (checkedActivityINPUTS.length > 0) {
-        console.log("activity true")
         return true
     } else {
-        console.log("activity false")
         insertErrorMessage($(activitySection).find('div'), "At least one activity must be selected")
         return false
     }
 }
 
 const ccNumValidation = () => { 
-    if (/^[0-9]{13,16}/.test(ccNum.val())) {
-        console.log("cc true")
+    if (/^[0-9]{13,16}$/.test(ccNum.val())) {
         return true
     } else {
-        console.log("cc false")
         insertErrorMessage(ccNum, "The credit card number must be between 13 and 16 digits")
         return false
     }
 }
 
 const zipCodeValidation = () => {
-    if (/^[0-9]{5}/.test(zipCode.val())) {
-        console.log("zip true")
+    if (/^[0-9]{5}$/.test(zipCode.val())) {
         return true
     } else {
-        console.log("zip false")
         insertErrorMessage(zipCode, "The zip code must be 5 digits")
         return false
     }
 }
 const cvvValidation = () => {
     if (/^[0-9]{3}$/.test(cvvNum.val())) {
-        console.log("cvv true")
         return true
     } else {
-        console.log("cvv false")
         insertErrorMessage(cvvNum, "The CVV number must be 3 digits")
         return false
     }
@@ -249,6 +260,7 @@ const creditCardValidation = () => {
 
 
 const insertErrorMessage = (element, message) => {
+    $(element).next(".error-message").remove()
     element.after($('<div class="error-message" style="background-color:red;text-align:center;padding:2% 5%;border-radius:20px;color:white;margin:0 auto;"></div>').text(message));
     element.css("border-color", "red")
 }
